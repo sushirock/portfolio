83286a197b430e9630a635762708d8328b5a415f
sushirock <86393996+sushirock@users.noreply.github.com>
2021-08-11 20:04:13 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,225 @@
+
+
+
+
+//テキストのカウントアップ+バーの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+	easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+	duration: 1000,//時間指定(1000＝1秒)
+	strokeWidth: 0.2,//進捗ゲージの太さ
+	color: '#555',//進捗ゲージのカラー
+	trailWidth: 0.2,//ゲージベースの線の太さ
+	trailColor: '#bbb',//ゲージベースの線のカラー
+	text: {//テキストの形状を直接指定				
+		style: {//天地中央に配置
+			position: 'absolute',
+			left: '50%',
+			top: '50%',
+			padding: '0',
+			margin: '-30px 0 0 0',//バーより上に配置
+			transform:'translate(-50%,-50%)',
+			'font-size':'1rem',
+			color: '#fff',
+		},
+		autoStyleContainer: false //自動付与のスタイルを切る
+	},
+	step: function(state, bar) {
+		bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+	}
+});
+
+//アニメーションスタート
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+	$("#splash_text").fadeOut(10);//フェイドアウトでローディングテキストを削除
+	$(".loader_cover-up").addClass("coveranime");//カバーが上に上がるクラス追加
+	$(".loader_cover-down").addClass("coveranime");//カバーが下に下がるクラス追加
+	$("#splash").fadeOut();//#splashエリアをフェードアウト
+});
+
+
+
+
+
+
+var unit = 100,
+canvasList, // キャンバスの配列
+info = {}, // 全キャンバス共通の描画情報
+colorList; // 各キャンバスの色情報
+
+/**
+ * Init function.
+ * 
+ * Initialize variables and begin the animation.
+ */
+function init() {
+	info.seconds = 0;
+    info.t = 0;
+	canvasList = [];
+    colorList = [];
+    // canvas1個めの色指定
+    canvasList.push(document.getElementById("waveCanvas"));
+    colorList.push(['#333', '#444', '#555', '#666', '#777']);//重ねる波線の色設定
+    
+	
+	// 各キャンバスの初期化
+	for(var canvasIndex in canvasList) {
+		var canvas = canvasList[canvasIndex];
+        canvas.width = document.documentElement.clientWidth; //Canvasのwidthをウィンドウの幅に合わせる
+        canvas.height = 200;//波の高さ
+        canvas.contextCache = canvas.getContext("2d");
+    }
+    // 共通の更新処理呼び出し
+	update();
+}
+
+function update() {
+	for(var canvasIndex in canvasList) {
+		var canvas = canvasList[canvasIndex];
+        // 各キャンバスの描画
+        draw(canvas, colorList[canvasIndex]);
+    }
+    // 共通の描画情報の更新
+    info.seconds = info.seconds + .014;
+    info.t = info.seconds*Math.PI;
+    // 自身の再起呼び出し
+    setTimeout(update, 35);
+}
+
+/**
+ * Draw animation function.
+ * 
+ * This function draws one frame of the animation, waits 20ms, and then calls
+ * itself again.
+ */
+function draw(canvas, color) {
+	// 対象のcanvasのコンテキストを取得
+    var context = canvas.contextCache;
+    // キャンバスの描画をクリア
+    context.clearRect(0, 0, canvas.width, canvas.height);
+	
+    //波線を描画 drawWave(canvas, color[数字（波の数を0から数えて指定）], 透過, 波の幅のzoom,波の開始位置の遅れ )
+    drawWave(canvas, color[0], 0.8, 3, 0);
+	drawWave(canvas, color[1], 0.5, 4, 0);
+	drawWave(canvas, color[2], 0.3, 1.6, 0);
+	drawWave(canvas, color[3], 0.2, 3, 100);
+	drawWave(canvas, color[4], 0.5, 1.6, 250);
+}
+
+/**
+ * 波を描画
+ * drawWave(色, 不透明度, 波の幅のzoom, 波の開始位置の遅れ)
+ */
+function drawWave(canvas, color, alpha, zoom, delay) {
+	var context = canvas.contextCache;
+    context.strokeStyle = color;//線の色
+	context.lineWidth = 1;//線の幅
+    context.globalAlpha = alpha;
+    context.beginPath(); //パスの開始
+    drawSine(canvas, info.t / 0.5, zoom, delay);
+    context.stroke(); //線
+}
+
+/**
+ * Function to draw sine
+ * 
+ * The sine curve is drawn in 10px segments starting at the origin. 
+ * drawSine(時間, 波の幅のzoom, 波の開始位置の遅れ)
+ */
+function drawSine(canvas, t, zoom, delay) {
+	var xAxis = Math.floor(canvas.height/2);
+    var yAxis = 0;
+    var context = canvas.contextCache;
+    // Set the initial x and y, starting at 0,0 and translating to the origin on
+    // the canvas.
+    var x = t; //時間を横の位置とする
+    var y = Math.sin(x)/zoom;
+    context.moveTo(yAxis, unit*y+xAxis); //スタート位置にパスを置く
+	
+    // Loop to draw segments (横幅の分、波を描画)
+    for (i = yAxis; i <= canvas.width + 10; i += 10) {
+		x = t+(-yAxis+i)/unit/zoom;
+        y = Math.sin(x - delay)/3;
+        context.lineTo(i, unit*y+xAxis);
+    }
+}
+
+init();
+
+
+
+
+
+
+
+
+// glowAnimeにglowというクラス名を付ける定義
+function GlowAnimeControl() {
+	$('.glowAnime').each(function () {
+		var elemPos = $(this).offset().top - 50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight) {
+			$(this).addClass("glow");
+			
+		} else {
+			$(this).removeClass("glow");
+		}
+	});
+}
+
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	//spanタグを追加する
+	var element = $(".glowAnime");
+	element.each(function () {
+		var text = $(this).text();
+		var textbox = "";
+		text.split('').forEach(function (t, i) {
+			if (t !== " ") {
+				if (i < 10) {
+					textbox += '<span style="animation-delay: .' + i + 's;">' + t + '</span>';
+				} else {
+					var n = i / 10;
+					textbox += '<span style="animation-delay: ' + n + 's;">' + t + '</span>';
+				}
+				
+			} else {
+				textbox += t;
+			}
+		});
+		$(this).html(textbox);
+	});
+	
+    GlowAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+
+$(function(){
+	　$(window).scroll(function (){
+		$('.effect-fade').each(function(){
+			var elemPos = $(this).offset().top;
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll > elemPos - windowHeight){
+				$(this).addClass('effect-scroll');
+			}
+		});
+	　});
+	});
+
+
+
+
+$(function(){
+	$("a[href^=#]:not([href=#])").click(function(){
+
+		var target = $($(this).autoStyleContainer("href")).offset().top;
+
+		$("html,body").animate({scrollTop: target},500);
+
+
+		return false;
+	})
+})
